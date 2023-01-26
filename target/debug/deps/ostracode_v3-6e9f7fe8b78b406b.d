102f4f10d2d15a3974799da042113d9dfc4a48af
Salanoid <salanoid@gmail.com>
2023-01-26 13:46:10 -0800

    remove target folder

--- target/debug/deps/ostracode_v3-6e9f7fe8b78b406b.d
+++
@@ -1,5 +0,0 @@
-/apps/rust/ostracode-v3/target/debug/deps/ostracode_v3-6e9f7fe8b78b406b.rmeta: src/main.rs
-
-/apps/rust/ostracode-v3/target/debug/deps/ostracode_v3-6e9f7fe8b78b406b.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/ostracode_v3-84aa32b9700445c8.d
+++
@@ -1,5 +0,0 @@
-/apps/rust/ostracode-v3/target/debug/deps/ostracode_v3-84aa32b9700445c8.rmeta: src/lib.rs
-
-/apps/rust/ostracode-v3/target/debug/deps/ostracode_v3-84aa32b9700445c8.d: src/lib.rs
-
-src/lib.rs:
